2f44e0b6908624592a97714e5d36fb14e18a125c
Hicckaro <55195835+Hicckaro@users.noreply.github.com>
2019-11-19 09:46:32 -0500

    Add files via upload

---
+++ project/site1/main.js
@@ -0,0 +1,11 @@
+const addHoverEffect = (elmnt) => {
+	elmnt.classList.add('link--active');
+};
+
+const removeHoverEffect = (elmnt) => {
+	elmnt.classList.remove('link--active');
+};
+
+// JQUERY
+
+$(() => {});
