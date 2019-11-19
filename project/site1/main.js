0da7581ebc83686f2948d76066f0f3be07f17320
Hicckaro <55195835+Hicckaro@users.noreply.github.com>
2019-11-19 18:53:42 -0500

    Add files via upload

--- project/site1/main.js
+++ project/site1/main.js
@@ -1,11 +1,48 @@
 const addHoverEffect = (elmnt) => {
-	elmnt.classList.add('link--active');
+	if (elmnt.dataset.active == 'false') {
+		elmnt.classList.add('link--active');
+	}
 };
 
 const removeHoverEffect = (elmnt) => {
-	elmnt.classList.remove('link--active');
+	if (elmnt.dataset.active == 'false') {
+		elmnt.classList.remove('link--active');
+	}
+};
+
+const changeClass = (elmnt) => {
+	const links = document.querySelectorAll('nav ul a');
+	for (let i = 0; i < links.length; i++) {
+		links[i].classList.remove('link--active');
+		links[i].dataset.active = 'false';
+	}
+
+	elmnt.classList.add('link--active');
+	elmnt.dataset.active = 'true';
 };
 
 // JQUERY
 
-$(() => {});
+$(() => {
+	$('.hero-pic').fadeIn(1000);
+
+	$('header').delay(100).animate({
+		left: '0'
+	}, 1000, () => {
+		$('header');
+	});
+
+	$('.main-text').delay(1200).animate({
+		paddingLeft: 0,
+		opacity: 1
+	}, 1000, () => {
+		$('.main-text').show();
+	});
+
+	$('.sub-text').delay(1500).animate({
+		paddingLeft: '5px',
+		opacity: 0.8
+	}, 1000, () => {
+		$('.sub-text').show();
+	});
+});
